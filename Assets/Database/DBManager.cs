f2ff56c7e44856d02b0cdfe664f170331ce734ba
Diana <dchumpitazm@gmail.com>
2018-10-08 10:46:01 -0500

    Initial commit

---
+++ Assets/Database/DBManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Firebase.Auth;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DBManager : MonoBehaviour {
+
+
+
+	public InputField email, password;
+	// Use this for initialization
+	public Text log;
+
+	public void LoginButtonPressed(){
+		FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(email.text,password.text).
+		ContinueWith((response)=>{
+			
+			if(response.IsCanceled){
+				Debug.LogError("LOGIN CANCELADO");
+				return;
+			}
+			if(response.IsFaulted){
+				Debug.LogError("Ocurrio un error");
+				log.text = "USUARIO O CONTRASENA INCORRECTOS";
+				return;
+			}
+
+			FirebaseUser user = response.Result;
+			Debug.LogFormat("Usuario logueado: {0}  {1}", user.Email, user.UserId);
+			log.text= "USUARIO LOGUEADO";
+			
+		});
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
